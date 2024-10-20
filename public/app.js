b8d119823afb4e403e991434d0686f8176a191c2
Dedy Setiadi Pramadan <34924797+gdslhack@users.noreply.github.com>
2024-10-20 19:42:06 +0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -8,14 +8,18 @@ document.getElementById('cekRekeningForm').addEventListener('submit', async (eve
         const response = await fetch(`/cek-rekening?bankCode=${bankCode}&accountNumber=${accountNumber}`);
         const data = await response.json();
 
-        const resultHTML = `
-            <h2>Hasil Cek Rekening</h2>
-            <p><strong>Bank Code:</strong> ${data.bankcode}</p>
-            <p><strong>Account Number:</strong> ${data.accountnumber}</p>
-            <p><strong>Account Name:</strong> ${data.accountname}</p>
-        `;
-
-        document.getElementById('responseContainer').innerHTML = resultHTML;
+        // Periksa jika data.status true dan data.data ada
+        if (data.status && data.data) {
+            const resultHTML = `
+                <h2>Hasil Cek Rekening</h2>
+                <p><strong>Bank Code:</strong> ${data.data.bankcode}</p>
+                <p><strong>Account Number:</strong> ${data.data.accountnumber}</p>
+                <p><strong>Account Name:</strong> ${data.data.accountname}</p>
+            `;
+            document.getElementById('responseContainer').innerHTML = resultHTML;
+        } else {
+            document.getElementById('responseContainer').innerHTML = 'Data tidak ditemukan';
+        }
     } catch (error) {
         console.error('Error fetching data:', error);
         document.getElementById('responseContainer').innerHTML = 'Error fetching data';
